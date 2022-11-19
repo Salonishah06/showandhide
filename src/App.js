c08f3cd37bc1a5530284268f96d310b782897a52
Saloni <salonishah306@gmail.com>
2022-11-19 18:01:32 +0530

    show and hide component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import  React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends Component {
+  
+    constructor(props){
+      super(props)
+      this.state={show:true}
+      this.toggleDiv =this.toggleDiv.bind(this)
+
+      
+    }
+
+    toggleDiv = () => {
+      const {show} = this.state;
+      this.setState({show: !show})
+    }
+    render(){
+      return (
+        <div className="App">
+          <div className="App-header">
+            
+            <p>
+              Hey
+            </p>
+          </div>
+          <div className="App-Intro">
+            <br />
+            <button onClick={ this.toggleDiv}>Toggle div</button>
+            <br />
+            <br />
+            {this.state.show && <Box/>}
+          </div>
+        </div>
+      );
+    }
+    }
+
+    class Box extends Component{
+      render(){return(
+      <div>This is a div</div>)
+      }
+    }
 
 export default App;
